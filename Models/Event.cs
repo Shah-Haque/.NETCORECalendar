75d7857ce0d93f2c66063eaf7dc9c9f987903c16
Shah Haque <FayyadhHaque@Hotmail.com>
2022-06-12 16:41:01 +0100

    Added two models Events and Locations links to db context

---
+++ Models/Event.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcoreCalendar.Models
+{
+    public class Event
+    {
+        /// <summary>
+        /// Id for event
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of event
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description of Event
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Starting time of event
+        /// </summary>
+        public DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// Ending time of event
+        /// </summary>
+        public DateTime EndTime { get; set; }
+
+        //Relational Data
+        /// <summary>
+        /// This gets the location through a 1:many basis
+        /// </summary>
+        public virtual Location Location { get; set; }
+
+    }
+}
---
+++ Models/Location.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcoreCalendar.Models
+{
+    public class Location
+    {
+        /// <summary>
+        /// Id of location
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of Location
+        /// </summary>
+        public string Name { get; set; }
+
+        //Relational Data
+        /// <summary>
+        /// This connects with the Event model
+        /// </summary>
+        public virtual ICollection<Event> Events { get; set; }
+    }
+}
